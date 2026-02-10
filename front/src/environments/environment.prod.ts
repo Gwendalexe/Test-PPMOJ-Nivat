a3727fd97967fc93e93d6b98d85330e4f5d65bd9
frank <frank.delvoye@etu.univ-nantes.fr>
2026-02-10 22:48:54 +0100

    fix

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.ppmoj.fr/dHJhbnNmb3Jtw6llbW9qZXR0ZV9wcm9k2',
-  frontUrl: 'https://www.ppmoj.fr/',
-  stripePublicKey:
-    'pk_live_51Qx5R9Dx4E6syhvW5gj2pzCLiHj9P5u2cQqrEder5Y54vuGaIBE4JARGGINRmgwmGxoCv1JoVwri3ZuyKHrhaZed00eP41j28A', //TODO : mettre la vraie clé publique en prod
+  apiUrl: 'https://gwendalexe.pythonanywhere.com',
 };
